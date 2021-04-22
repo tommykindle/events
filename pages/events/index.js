f57ba802901d84f4b6f7f50866c32511880a485a
tommykindle <tommykindle@Tommys-MacBook-Air.local>
2021-04-21 21:19:59 -0700

    finished project

---
+++ pages/events/index.js
@@ -0,0 +1,23 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
